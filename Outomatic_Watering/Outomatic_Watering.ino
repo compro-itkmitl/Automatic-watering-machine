d0d4cfebf518ce865719505373f88154c04214c4
Dechapon Lerdsurut <60070027@kmitl.ac.th>
2018-04-22 14:50:16 +0700

    All night

--- Outomatic_Watering/Outomatic_Watering.ino
+++ Outomatic_Watering/Outomatic_Watering.ino
@@ -12,11 +12,14 @@
   #define PIN8_PumpSoil 8  //pin 8
   #define PIN9_PumpSpray 9 //pin 9
 
-  SoftwareSerial ArduinoSerial(10,11); //(RX,TX)
+  SoftwareSerial ArduinoSerial(6,10); //(RX,TX)
   LiquidCrystal_I2C lcd(0x3F,2,1,0,4,5,6,7,3,POSITIVE);
 
+  int wait;
+
+
   unsigned long timer;
-  //unsigned long timer2;
+  unsigned long timer2;
   
   DHT dht;
   
@@ -27,8 +30,8 @@ void setup(){
   //Setup All The Arduino Pin
   pinMode(PIN8_PumpSoil, OUTPUT);
   pinMode(PIN9_PumpSpray, OUTPUT);
-  pinMode(10, INPUT);
-  pinMode(11, OUTPUT);
+  pinMode(6, INPUT);
+  pinMode(10, OUTPUT);
 
   dht.setup(5);
 
@@ -39,7 +42,7 @@ void setup(){
   delay(1000);
 
   timer = 0;
-  //timer2 = 0;
+  timer2 = 0;
 
   lcd.begin (16,2);
   lcd.setCursor(0,0);
@@ -55,13 +58,16 @@ void loop() {
   float temperature_air = dht.getTemperature();
 
   ArduinoSerial.print(humid_1);
+  ArduinoSerial.print('\n');
   ArduinoSerial.print(humid_2);
+  ArduinoSerial.print('\n');
   ArduinoSerial.print(percentage());
+  ArduinoSerial.print('\n');
   ArduinoSerial.print(temperature_air);
   ArduinoSerial.print('\n');
   
-  if(percentage() <= 2 && percentage() >= 0){
-    for(int wait=0; wait<800; wait++){
+  if(percentage() <= 2.25 && percentage() >= 0){
+    for(wait=0; wait<800; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -73,7 +79,7 @@ void loop() {
       Serial.println(wait);
     }
     
-    for(int wait=0; wait<3000; wait++){
+    for(wait=0; wait<3000; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -86,8 +92,8 @@ void loop() {
     }
   }
   
-  else if (percentage() <= 41 && percentage() > 2){
-    for(int wait=0; wait<800; wait++){
+  else if (percentage() <= 41.25 && percentage() >= 2.35){
+    for(wait=0; wait<800; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -98,7 +104,7 @@ void loop() {
       digitalWrite(PIN8_PumpSoil,LOW);
       Serial.println(wait);
     }
-    for(int wait=0; wait<3000; wait++){
+    for(wait=0; wait<3000; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -111,8 +117,8 @@ void loop() {
     }
   }
   
-  else if (percentage() <= 50 && percentage() > 41){
-    for(int wait=0; wait<800; wait++){
+  else if (percentage() <= 50.05 && percentage() >= 41.35){
+    for(wait=0; wait<800; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -124,7 +130,7 @@ void loop() {
       Serial.println(wait);
     }
     
-    for(int wait=0; wait<3000; wait++){
+    for(wait=0; wait<3000; wait++){
       float temperature_air = dht.getTemperature();
       if((millis() - timer ) >= 1000){
         lcd.clear();
@@ -137,7 +143,7 @@ void loop() {
     }
   }
   
-  else if (percentage() <= 56 && percentage() > 50){
+  else if (percentage() <= 56.01 && percentage() >= 50.15){
     if((millis() - timer ) >= 1000){
       lcd.clear();
       print_humid("Good : ", percentage(), "%");
@@ -158,23 +164,23 @@ void loop() {
   } //End Algo Watering Soil
 
   //Algo Watering Spray
-  //if((millis() - timer2 ) >= 3000){
-    //if(temperature_air > 36){
-      //for(int wait=0; wait<10000; wait++){
-        //temperature_air = dht.getTemperature();
-        //if((millis() - timer ) >= 1000){
-          //lcd.clear();
-          //print_humid("Normal : ", percentage(), "%");
-          //print_temp("Temp : ", temperature_air, "C");
-          //timer = millis();
-        //}
-        //digitalWrite(PIN9_PumpSpray,LOW);
-        //Serial.println(wait);
-      //}
-      //digitalWrite(PIN9_PumpSpray,HIGH);
-    //}
-    //timer2 = millis();
-  //} //End Algo Watering Spray
+  if((millis() - timer2 ) >= 3000){
+    if(temperature_air > 36){
+      for(wait=0; wait<10000; wait++){
+        temperature_air = dht.getTemperature();
+        if((millis() - timer ) >= 1000){
+          lcd.clear();
+          print_humid("Normal : ", percentage(), "%");
+          print_temp("Temp : ", temperature_air, "C");
+          timer = millis();
+        }
+        digitalWrite(PIN9_PumpSpray,LOW);
+        Serial.println(wait);
+      }
+      digitalWrite(PIN9_PumpSpray,HIGH);
+    }
+    timer2 = millis();
+  } //End Algo Watering Spray
   delay(500);
 }
 
--- wifi_NodeMCU/wifi_NodeMCU.ino
+++ wifi_NodeMCU/wifi_NodeMCU.ino
@@ -1,6 +1,6 @@
+#include <SoftwareSerial.h>
 #include <FirebaseArduino.h>
 #include <ESP8266WiFi.h>
-#include <SoftwareSerial.h>
 
 #define WIFI_ID        "Lerdsurut"
 #define WIFI_PASSWORD  "5316038285"
@@ -12,9 +12,6 @@ SoftwareSerial Node(D1,D2); //(RX,TX)
 void setup() {
   Serial.begin(115200);
   Node.begin(4800);
-
-  pinMode(D1, INPUT);
-  pinMode(D2, OUTPUT);
   
   WiFi.begin(WIFI_ID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -27,29 +24,34 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   Firebase.begin(FIREBASE_HOST, FIREBASE_KEY);
-  Firebase.setInt("humid 1",0);
-  Firebase.setInt("humid 2",0);
-  Firebase.setInt("temperature",0);
-  
+  Firebase.setFloat("Humid 1",0);
+  Firebase.setFloat("Humid 2",0);
+  Firebase.setFloat("Percentage",0);
+  Firebase.setFloat("Temperature",0);
 }
 
 void loop() {
   while(Node.available() > 0){
-    int humid_1 = Node.parseInt();
-    int humid_2 = Node.parseInt();
-    float temperature_air = Node.parseFloat();
+    float humid_1 = Node.parseFloat();
+    float humid_2 = Node.parseFloat();
     float percentage = Node.parseFloat();
+    float temperature_air = Node.parseFloat();
+    
     
     if(Node.read() == '\n'){
-      Firebase.setInt("humid 1",humid_1);
-      Firebase.setInt("humid 2",humid_2);
+      Firebase.setFloat("Humid 1",humid_1);
+      Firebase.setFloat("Humid 2",humid_2);
       Firebase.setFloat("Percentage",percentage);
-      Firebase.setFloat("temperature",temperature_air);
+      Firebase.setFloat("Temperature",temperature_air);
 
-      Serial.print("Humid 1: ");Serial.println(humid_1);
-      Serial.print("Humid 2: ");Serial.println(humid_2);
-      Serial.print("Percentage: ");Serial.println(percentage);
-      Serial.print("Temperature: ");Serial.println(temperature_air);
+      Serial.print("Humid 1: ");
+      Serial.println(humid_1);
+      Serial.print("Humid 2: ");
+      Serial.println(humid_2);
+      Serial.print("Percentage: ");
+      Serial.println(percentage);
+      Serial.print("Temperature: ");
+      Serial.println(temperature_air);
     }
   }
   delay(100);
